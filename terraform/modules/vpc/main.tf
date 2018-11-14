ef43477df74c4bb0384d06c18318e89268d2c074
Ilya Nikolskiy <inikolskiy@gmail.com>
2018-11-14 21:52:50 +0300

    Finished roles + vault

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -7,8 +7,24 @@ allow {
     protocol = "tcp"
     ports    = ["22"]
   }
+network = "default"
 
 target_tags = ["reddit-app","reddit-db"]
+
+source_ranges = "${var.source_ranges}"
+}
+resource "google_compute_firewall" "allow_http" {
+  name = "allow-http-default"
+
+network = "default"
+
+allow {
+    protocol = "tcp"
+    ports    = ["80"]
+}
+network = "default"
+
+target_tags = ["reddit-app"]
 
 source_ranges = "${var.source_ranges}"
 }
